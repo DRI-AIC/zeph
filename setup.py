5c10a249b967bf03dd96cf50bca4d9422c09723f
Andrew Vitale <vitale232@gmail.com>
2015-11-13 11:57:26 -0800

    Or intoxicated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='zeph',
-      version='0.0.1',
+      version='0.0.15',
       description='Geographic arrays in Python',
       url='https://github.com/vitale232/zeph',
       author='Charles Morton and Andrew Vitale',
